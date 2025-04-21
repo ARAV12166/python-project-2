f8be784bcace521d31b10b4fe3519c0550ec027f
ARAV12166 <rt355034@gmail.com>
2025-04-21 10:06:17 +0530

    Add files via upload

---
+++ notebooks/data_cleaning.py
@@ -0,0 +1,3 @@
+# Data Cleaning using pandas and numpy
+import pandas as pd
+import numpy as np
---
+++ notebooks/eda.py
@@ -0,0 +1,3 @@
+# EDA using matplotlib and seaborn
+import matplotlib.pyplot as plt
+import seaborn as sns
